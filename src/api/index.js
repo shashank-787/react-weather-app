80a62bba3be6851f4099ba62a5741b5dd4f1c858
Shashank <101469744+shashank-787@users.noreply.github.com>
2025-08-18 13:00:25 +0530

    .env bug fix

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,13 @@ const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 const baseUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}` 
 
 export async function getWeatherDataForCity(city){
+    
     const res = await fetch(`${baseUrl}&q=${city}&aqi=yes`)
     return await res.json()
-}  
+}   
 
 export async function getWeatherDataForLocation(lat, lon){
+    console.log(baseUrl)
     const res = await fetch(`${baseUrl}&q=${lat},${lon}&aqi=yes`)
     return await res.json()
 }  
